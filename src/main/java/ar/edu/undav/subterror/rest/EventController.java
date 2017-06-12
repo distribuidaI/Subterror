c3bf5d37d270b38e6509e10668bf8dfb2df49577
Leonardo Guanco <leonardo.guanco@gmail.com>
2017-06-11 23:55:57 -0300

    Se creo algunos rest controller

--- src/main/java/ar/edu/undav/subterror/rest/EventController.java
+++ src/main/java/ar/edu/undav/subterror/rest/EventController.java
@@ -3,17 +3,28 @@ package ar.edu.undav.subterror.rest;
 /**
  * Created by leo on 6/11/17.
  */
+import ar.edu.undav.subterror.model.Event;
 import ar.edu.undav.subterror.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/allevent")
-public class HelloController
+@RequestMapping(value = "/events")
+public class EventController
 {
-    EventRepository eventRepository;
+    private EventRepository eventRepository;
 
     @Autowired
-    public 
+    public EventController(EventRepository eventRepository){
+        this.eventRepository = eventRepository;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Event> getAll(){
+        return this.eventRepository.findAll();
+    }
 }
--- src/main/java/ar/edu/undav/subterror/rest/EventTypeController.java
+++ src/main/java/ar/edu/undav/subterror/rest/EventTypeController.java
@@ -1,7 +1,31 @@
 package ar.edu.undav.subterror.rest;
 
+import ar.edu.undav.subterror.model.EventType;
+import ar.edu.undav.subterror.repository.EventTypeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by leo on 6/11/17.
  */
-public class EvenTypeController {
+
+@RestController
+@RequestMapping(value = "/eventtype")
+public class EventTypeController {
+
+    private EventTypeRepository eventTypeRepository;
+
+    @Autowired
+    public EventTypeController(EventTypeRepository eventTypeRepository){
+        this.eventTypeRepository = eventTypeRepository;
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<EventType> getAll(){
+        return this.eventTypeRepository.findAll();
+    }
 }
