16daf743be9ebe3c934199525db242459f03c1f5
Leonardo Guanco <leonardo.guanco@gmail.com>
2017-06-11 23:55:23 -0300

    Se crearon algunas entidades

--- src/main/java/ar/edu/undav/subterror/repository/EventRepository.java
+++ src/main/java/ar/edu/undav/subterror/repository/EventRepository.java
@@ -1,7 +1,14 @@
 package ar.edu.undav.subterror.repository;
 
+import ar.edu.undav.subterror.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by leo on 6/11/17.
  */
-public interface EventRepository {
+
+@Repository
+public interface EventRepository extends JpaRepository<Event,Long>{
+
 }
--- src/main/java/ar/edu/undav/subterror/repository/EventTypeRepository.java
+++ src/main/java/ar/edu/undav/subterror/repository/EventTypeRepository.java
@@ -1,7 +1,14 @@
 package ar.edu.undav.subterror.repository;
 
+import ar.edu.undav.subterror.model.EventType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by leo on 6/11/17.
  */
-public interface EventTypeRepository {
+
+@Repository
+public interface EventTypeRepository extends JpaRepository<EventType,Long>{
+
 }
