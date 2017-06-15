cd5bc58226b106217805156398e3e0850e04a1e0
Leonardo Guanco <leonardo.guanco@gmail.com>
2017-06-15 00:09:42 -0300

    Se agregaron algunos test hacia los repositorios, lo ideal seria hacerlo hacia los servicios

--- src/test/java/ar/edu/undav/subterror/SubterrorApplicationTests.java
+++ src/test/java/ar/edu/undav/subterror/SubterrorApplicationTests.java
@@ -1,16 +1,90 @@
 package ar.edu.undav.subterror;
 
+import ar.edu.undav.subterror.model.Event;
+import ar.edu.undav.subterror.model.EventType;
+import ar.edu.undav.subterror.model.Station;
+import ar.edu.undav.subterror.repository.EventRepository;
+import ar.edu.undav.subterror.repository.EventTypeRepository;
+import ar.edu.undav.subterror.repository.StationRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import javax.persistence.EntityManager;
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SubterrorApplicationTests {
 
+	@Autowired
+	private EntityManager entityManager;
+
+	@Autowired
+	private EventTypeRepository eventTypeRepository;
+
+	@Autowired
+	private StationRepository stationRepository;
+
+	@Autowired
+	private EventRepository eventRepository;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() {}
+
+	@Test
+	@Transactional
+	public void testEventType(){
+		EventType eventType = new EventType("Demora");
+		entityManager.persist(eventType);
+		entityManager.flush();
+
+		EventType found = eventTypeRepository.findByDescription(eventType.getDescription());
+
+		assertThat(found.getDescription(),equalTo(eventType.getDescription()));
+	}
+
+	@Test
+	@Transactional
+	public void testStation(){
+		Station estacion = new Station("Linea D", "Congreso");
+		entityManager.persist(estacion);
+		entityManager.flush();
+
+		Station found = stationRepository.findByStation(estacion.getStation());
+
+		assertThat(found.getLine(),equalTo(estacion.getLine()));
+	}
+
+	@Test
+	@Transactional
+	public void testEvent(){
+		Date fecha = new Date();
+		String hora = "00:00:00";
+		String sense = "Sentido a sarasa";
+		EventType eventType = new EventType("Demora");
+		entityManager.persist(eventType);
+		entityManager.flush();
+
+		Station estacion = new Station("Linea D", "Congreso");
+		entityManager.persist(estacion);
+		entityManager.flush();
+
+		Event event = new Event(fecha,hora,sense,eventType,estacion);
+
+		entityManager.persist(event);
+		entityManager.flush();
+
+		Event found = eventRepository.findByStation(event.getStation());
+
+		assertThat(found.getStation(),equalTo(estacion.getStation()));
 	}
 
 }
